de677c2905c03125b3a6a2959e3449389e0d84c2
IvanHdzF <ivan083198@hotmail.com>
2024-03-14 10:41:19 -0700

    Deleted files in build folder since they are untracked now

---
+++ DemoMakeFile/build/CMakeFiles/firmware.dir/DependInfo.cmake
@@ -0,0 +1,119 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/startup_stm32f446xx.s" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/startup_stm32f446xx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F446xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../"
+  "../BlueNRG-2/Target"
+  "../Core/Inc"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Middlewares/ST/BlueNRG-2/hci/hci_tl_patterns/Basic"
+  "../Middlewares/ST/BlueNRG-2/includes"
+  "../Middlewares/ST/BlueNRG-2/utils"
+  "../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2"
+  "../Middlewares/Third_Party/FreeRTOS/Source/include"
+  "../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/BlueNRG-2/Target/hci_tl_interface.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/BlueNRG-2/Target/hci_tl_interface.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Core/Src/Init.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Core/Src/Init.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Core/Src/Keyboard.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Core/Src/Keyboard.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Core/Src/StateMachine.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Core/Src/StateMachine.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Core/Src/callbacks.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Core/Src/callbacks.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Core/Src/freertos.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Core/Src/freertos.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Core/Src/kbd_process.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Core/Src/kbd_process.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Core/Src/main.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Core/Src/main.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Core/Src/services.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Core/Src/services.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Core/Src/sm.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Core/Src/sm.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Core/Src/stm32f4xx_hal_msp.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Core/Src/stm32f4xx_hal_msp.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Core/Src/stm32f4xx_hal_timebase_tim.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Core/Src/stm32f4xx_hal_timebase_tim.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Core/Src/stm32f4xx_it.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Core/Src/stm32f4xx_it.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Core/Src/stm32f4xx_nucleo_bus.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Core/Src/stm32f4xx_nucleo_bus.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Core/Src/syscalls.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Core/Src/syscalls.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Core/Src/system_stm32f4xx.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Core/Src/system_stm32f4xx.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Middlewares/ST/BlueNRG-2/hci/bluenrg1_devConfig.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Middlewares/ST/BlueNRG-2/hci/bluenrg1_devConfig.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Middlewares/ST/BlueNRG-2/hci/bluenrg1_events.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Middlewares/ST/BlueNRG-2/hci/bluenrg1_events.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Middlewares/ST/BlueNRG-2/hci/bluenrg1_events_cb.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Middlewares/ST/BlueNRG-2/hci/bluenrg1_events_cb.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Middlewares/ST/BlueNRG-2/hci/bluenrg1_hci_le.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Middlewares/ST/BlueNRG-2/hci/bluenrg1_hci_le.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Middlewares/ST/BlueNRG-2/hci/controller/bluenrg1_gap_aci.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Middlewares/ST/BlueNRG-2/hci/controller/bluenrg1_gap_aci.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Middlewares/ST/BlueNRG-2/hci/controller/bluenrg1_gatt_aci.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Middlewares/ST/BlueNRG-2/hci/controller/bluenrg1_gatt_aci.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Middlewares/ST/BlueNRG-2/hci/controller/bluenrg1_hal_aci.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Middlewares/ST/BlueNRG-2/hci/controller/bluenrg1_hal_aci.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Middlewares/ST/BlueNRG-2/hci/controller/bluenrg1_l2cap_aci.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Middlewares/ST/BlueNRG-2/hci/controller/bluenrg1_l2cap_aci.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Middlewares/ST/BlueNRG-2/hci/hci_tl_patterns/Basic/hci_tl.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Middlewares/ST/BlueNRG-2/hci/hci_tl_patterns/Basic/hci_tl.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Middlewares/ST/BlueNRG-2/utils/ble_list.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Middlewares/ST/BlueNRG-2/utils/ble_list.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Middlewares/Third_Party/FreeRTOS/Source/croutine.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Middlewares/Third_Party/FreeRTOS/Source/croutine.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Middlewares/Third_Party/FreeRTOS/Source/list.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Middlewares/Third_Party/FreeRTOS/Source/list.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Middlewares/Third_Party/FreeRTOS/Source/queue.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Middlewares/Third_Party/FreeRTOS/Source/queue.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Middlewares/Third_Party/FreeRTOS/Source/tasks.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Middlewares/Third_Party/FreeRTOS/Source/tasks.c.obj"
+  "/home/iahernandez/STM32-Keyboard/DemoMakeFile/Middlewares/Third_Party/FreeRTOS/Source/timers.c" "/home/iahernandez/STM32-Keyboard/DemoMakeFile/build/CMakeFiles/firmware.dir/Middlewares/Third_Party/FreeRTOS/Source/timers.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32F446xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../"
+  "../BlueNRG-2/Target"
+  "../Core/Inc"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Middlewares/ST/BlueNRG-2/hci/hci_tl_patterns/Basic"
+  "../Middlewares/ST/BlueNRG-2/includes"
+  "../Middlewares/ST/BlueNRG-2/utils"
+  "../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2"
+  "../Middlewares/Third_Party/FreeRTOS/Source/include"
+  "../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
